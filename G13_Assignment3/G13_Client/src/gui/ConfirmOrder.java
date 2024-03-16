b0c5520a605421f5063e9d6e3fe8e96259453095
khatebnor29 <khatebnor29@gmail.com>
2024-03-16 16:36:25 +0200

    ConfirmOrder Method The fxml and the controller

--- G13_Assignment3/G13_Client/src/gui/ConfirmOrder.java
+++ G13_Assignment3/G13_Client/src/gui/ConfirmOrder.java
@@ -1,5 +1,73 @@
 package gui;
 
-public class ConfirmOrder {
+import client.ClientController;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.sql.SQLException;
 
+import client.ChatClient;
+import client.ClientController;
+import client.ClientUI;
+import common.ChatIF;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import ocsf.server.ConnectionToClient;
+
+public class ConfirmOrder {
+	public static ClientController chat;
+	private static int itemIndex = 3;
+	
+	@FXML
+	private Label lblError;
+	
+	
+	@FXML
+	private Label labelGetParkNameDB,labelGetVisitorsNumDB,labelPriceShowPayNow,labelPriceShowPayLater,labelGetDayDB,labelGetMonthDB,labelGetTimeDB;
+    
+	
+	public void PayNow(ActionEvent event) throws Exception {
+		FXMLLoader loader = new FXMLLoader();
+		((Node)event.getSource()).getScene().getWindow().hide(); //hiding primary window
+		Stage primaryStage = new Stage();
+		Pane root = loader.load(getClass().getResource("/gui/Payment.fxml").openStream());		
+		
+	
+		Scene scene = new Scene(root);			
+		//scene.getStylesheets().add(getClass().getResource("/gui/Login.css").toExternalForm());
+		primaryStage.setTitle("Login page");
+
+		primaryStage.setScene(scene);		
+		primaryStage.show();
+		
+	}
+	
+	
+	public void Logout(ActionEvent event) throws Exception {
+		FXMLLoader loader = new FXMLLoader();
+		((Node)event.getSource()).getScene().getWindow().hide(); //hiding primary window
+		Stage primaryStage = new Stage();
+		Pane root = loader.load(getClass().getResource("/gui/VisitorHomePage.fxml").openStream());		
+		
+	
+		Scene scene = new Scene(root);			
+		//scene.getStylesheets().add(getClass().getResource("/gui/Login.css").toExternalForm());
+		primaryStage.setTitle("Login page");
+
+		primaryStage.setScene(scene);		
+		primaryStage.show();
+		
+	}
 }
