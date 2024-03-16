b0f3b9ec66f1d1e2ee47c8a22e0244ae8c6a34bc
AhmadAboJabal <ahmad@DESKTOP-JVQIJ8M>
2024-03-16 22:30:10 +0200

    Back Button Added

--- G13_Assignment3/G13_Client/src/gui/PaymentController.java
+++ G13_Assignment3/G13_Client/src/gui/PaymentController.java
@@ -2,15 +2,22 @@ package gui;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 public class PaymentController {
 	
 	@FXML
 	private Button btnPay = null;
+	@FXML
+	private Button btnBack = null;
 	@FXML
 	private ComboBox<String> cmbMonth;
 	@FXML
@@ -77,6 +84,19 @@ public class PaymentController {
         }
     }
 	
-	
-	
+	//Back to the previous page
+	public void goBackBtn(ActionEvent event) throws Exception {
+		FXMLLoader loader = new FXMLLoader();
+		((Node)event.getSource()).getScene().getWindow().hide(); //hiding primary window
+		Stage primaryStage = new Stage();
+		Pane root = loader.load(getClass().getResource("/gui/ConfirmOrder.fxml").openStream());		
+		
+		Scene scene = new Scene(root);			
+		scene.getStylesheets().add(getClass().getResource("/gui/ConfirmOrder.css").toExternalForm());
+		primaryStage.setTitle("Confirm Order page");
+
+		primaryStage.setScene(scene);		
+		primaryStage.show();
+		
+	}
 }
